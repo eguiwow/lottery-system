98beb1df3aecdd6bace1f94bdd9bd43945db4b00
Ander Eguiluz <ander.eguiluz@opendeusto.es>
2023-06-05 23:37:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ origin_date = datetime.strptime("2023-05-18", "%Y-%m-%d")
 
 def main():
  
-    # Populate database with -p parameter
+    # Populate database with -p flag
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', action='store_true', help='Populate the database')
     args = parser.parse_args()
