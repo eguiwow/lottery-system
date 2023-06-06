72ebab27f56ef61ca057b81b7197db2bf6631735
eguiwow <ander.eguiluz@opendeusto.es>
2023-06-06 13:40:35 +0200

    New function: -m for  lottery every minute

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 import time
-from schedule import run_pending
-from functions import do_at_midnight, register_user, create_lottery, populate_db
+from schedule import run_pending, every
+from functions import schedule_midnight, schedule_minute, do_every_minute, do_at_midnight, register_user, create_lottery, populate_db
 from __init__ import session
 import argparse
 
@@ -12,18 +12,28 @@ def main():
     # Populate database with -p parameter
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', action='store_true', help='Populate the database')
+    parser.add_argument('-m', action='store_true', help='Minute lottery mode')
     args = parser.parse_args()
-
+    
     if args.p:
         populate_db(session, origin_date)
 
+    if args.m:
+        # Do once to prove how it works
+        do_every_minute()
+        # Start the initial scheduling every minute
+        schedule_minute()
+    else:
+        # Do once to prove how it works
+        do_at_midnight()
+        # Start the initial scheduling at midnight        
+        schedule_midnight()
+        
     # Testing some functions...
+    # -------------------------
     # check_winning_ballot_by_date(session, origin_date)
     # today = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
     # create_lottery(session, "Loto" + str(today)[0:10], today)
-
-    # Start the initial scheduling
-    do_at_midnight()
 
     while True:
         # schedule for every day at midnight
@@ -33,4 +43,3 @@ def main():
 if __name__== "__main__":
     main()
     
-    # just testin'
